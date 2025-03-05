c9fbd8272a5c9948e86710767cb7aa9b1b1968a0
nunchyCurso03 <you@example.com>
2025-03-05 13:13:35 +0100

    proyecto creado

---
+++ src/main/java/es/santander/ascender/ejer008/Ejer008Application.java
@@ -0,0 +1,13 @@
+package es.santander.ascender.ejer008;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejer008Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejer008Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/santander/ascender/ejer008/Ejer008ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.santander.ascender.ejer008;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejer008ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
